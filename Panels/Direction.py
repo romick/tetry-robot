5f9556fe05f844c916c0acf3de0d7063ff534c52
romick <roman.milovanov@gmail.com>
2014-03-16 16:30:04 +0400

    Added middle direction buttons

--- Panels/Direction.py
+++ Panels/Direction.py
@@ -8,46 +8,33 @@ class DirectionPanel(wx.Panel):
         wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
         self.bot    = kwds['bot']
         self.runner = kwds['runner']
+        self.buttons = []
 
-        self.button_forward = wx.Button(self, wx.ID_ANY, ("forward"))
-        self.button_left = wx.Button(self, wx.ID_ANY, ("left"))
-        self.button_right = wx.Button(self, wx.ID_ANY, ("right"))
-        self.button_back = wx.Button(self, wx.ID_ANY, ("backward"))
+        b_metadata = {"forward-left":315, "forward": 0,"forward-right":45, "left":270, "right":90,"backward-left":225, "backward":180, "backward-right":135}
+        for button_label, angle in b_metadata.iteritems():
+            b = wx.Button(self, wx.ID_ANY, (button_label))
+            self.buttons.append(b)
+            b.SetMinSize((50, 50))
+            self.Bind(wx.EVT_BUTTON, lambda evt, an = angle: self.button_pressed(evt, an))
 
-        self.button_forward.SetMinSize((100, 100))
-        self.button_left.SetMinSize((100, 100))
-        self.button_right.SetMinSize((100, 100))
-        self.button_back.SetMinSize((100, 100))
 
         grid_sizer_1 = wx.FlexGridSizer(3, 3, 1, 1)
 
-        self.gaits = ["tripod", "wave", ""]
+        # self.gaits = ["tripod", "wave", ""]
 
-        grid_sizer_1.Add((60, 60), 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_1.Add(self.button_forward, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_1.Add((60, 60), 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_1.Add(self.button_left, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_1.Add((150, 150), 0, 0, 0)
-        grid_sizer_1.Add(self.button_right, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_1.Add((60, 60), 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_1.Add(self.button_back, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
-        grid_sizer_1.Add((60, 60), 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add(self.buttons[0], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add(self.buttons[1], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add(self.buttons[2], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add(self.buttons[3], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add((200, 200), 0, 0, 0)
+        grid_sizer_1.Add(self.buttons[4], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add(self.buttons[5], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add(self.buttons[6], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_1.Add(self.buttons[7], 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, 0)
 
         self.SetSizer(grid_sizer_1)
 
-        self.Bind(wx.EVT_BUTTON, self.forward_button_pressed, self.button_forward)
-        self.Bind(wx.EVT_BUTTON, self.left_button_pressed, self.button_left)
-        self.Bind(wx.EVT_BUTTON, self.right_button_pressed, self.button_right)
-        self.Bind(wx.EVT_BUTTON, self.back_button_pressed, self.button_back)
 
-    def forward_button_pressed(self, event):
-                    self.runner(self.bot.makeStep,0)
+    def button_pressed(self, event, angle):
+                    self.runner(self.bot.makeStep,angle)
 
-    def left_button_pressed(self, event):
-                    self.runner(self.bot.makeStep, 270)
-
-    def right_button_pressed(self, event):
-                    self.runner(self.bot.makeStep, 90)
-
-    def back_button_pressed(self, event):
-                    self.runner(self.bot.makeStep, 180)
