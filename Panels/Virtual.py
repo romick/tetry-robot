60c18cf9782a0a118504534d339c59eb577d0e20
romick <roman.milovanov@gmail.com>
2014-03-30 21:58:47 +0400

    No matplotlib

--- Panels/Virtual.py
+++ Panels/Virtual.py
@@ -1,8 +1,8 @@
 __author__ = 'roman_000'
 
 import wx
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
 
 
 class VirtualPanel(wx.Panel):
@@ -11,5 +11,5 @@ class VirtualPanel(wx.Panel):
         self.bot = kwds['bot']
         self.runner = kwds['runner']
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
