1b10b1f9352760eacda0812586be31d3c8c79304
romick <roman.milovanov@gmail.com>
2014-03-30 22:33:59 +0400

    Deleted irrelevant crawler firmware code

--- bug3/lib/opt_pcint.c
+++
@@ -1,6 +0,0 @@
-#define BUILDING_LIBRARY
-#include "lib_iopins.h"
-#include <pinChange.h>
-const uint8_t NUM_PCINT_PINS = 8;
-const IOPin* PROGMEM const PCINT_PINS[]={null,null,null,null,B4,B5,B6,B7};
-PIN_CHANGE pcCallbacks[8];
