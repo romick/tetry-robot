2b2ee162763c716ff3c7f472ff2b56ba4361011c
romick <roman.milovanov@gmail.com>
2015-01-09 23:49:09 +0300

    Preparations for web-based client-server architecture

--- cliTetry.py
+++ cliTetry.py
@@ -1,4 +1,30 @@
 __author__ = 'roman_000'
+import Crawler
 
-# import Crawler
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+class MainLoop():
+    """Bot logic"""
+
+    def __init__(self):
+        self.bot = Crawler.Controller(sender=self.dummysender)
+
+
+    def dummysender(self, **kwds):
+        print(kwds)
+
+@app.route('/')
+def hello_world():
+    # some = jsonify(loop.bot)
+    bot.sender(log="Hello!")
+
+    return render_template('Hello World!'
+
+if __name__ == '__main__':
+    loop = MainLoop()
+    bot = loop.bot
+    app.debug = True
+    app.run()
 
