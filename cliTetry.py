71a7bc407603a640dcfbb752464a355d9e5523ef
romick <roman.milovanov@gmail.com>
2015-01-10 04:42:53 +0300

    Added command_generator

--- cliTetry.py
+++ cliTetry.py
@@ -1,13 +1,22 @@
 __author__ = 'roman_000'
 import Crawler
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, abort
 
 app = Flask(__name__)
 
-class MainLoop():
-    """Bot logic"""
+class TaskQueue():
+    """
+    Placeholder for real task queue
+    """
+    def put_queue(self,**kwds):
+        return None
 
+
+class MainLoop():
+    """
+    Bot logic
+    """
     def __init__(self):
         self.bot = Crawler.Controller(sender=self.dummysender)
 
@@ -16,15 +25,35 @@ class MainLoop():
         print(kwds)
 
 @app.route('/')
-def hello_world():
-    # some = jsonify(loop.bot)
+def index():
     bot.sender(log="Hello!")
+    return render_template('index.html')
 
-    return render_template('Hello World!'
+@app.route('/tetry/api/1.0/tasks/', methods=['POST'])
+def add_task_to_queue():
+    #TODO: add code to save task to queue
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    q.put_queue(json=request.json)
+    return jsonify({'status':'saved'})
+
+@app.route('/tetry/api/1.0/tasks/<filter_group>', methods=['GET'])
+def list_available_tasks(filter_group):
+    tasks = {
+        'moves': ['example', 'test'],
+        'direction': ['forward', 'right', 'left', 'backward'],
+        'shiftbody': ['bodyforward', 'bodyright', 'bodyleft', 'bodybackward'],
+        'tiltbody': ['tiltforward', 'tiltright', 'tiltleft', 'tiltbackward']
+
+    }
+    list = tasks[filter_group]
+    bot.sender(log=list)
+    return render_template('commands.html', commands=list)
 
 if __name__ == '__main__':
     loop = MainLoop()
     bot = loop.bot
+    q = TaskQueue()
     app.debug = True
     app.run()
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,54 @@
 
+var tetry = {
+    init: function(){
+        $( "#tetry-commands-content" ).load( "/tetry/api/1.0/tasks/moves" );
+        tetry.add_event_handlers();
+    },
+
+    add_event_handlers: function(){
+        $( ".tetry-command-group" ).each( function() {
+            $(this).click(tetry.load_command_group());
+         });
+        $( ".tetry-command" ).each( function() {
+            $(this).click(tetry.send_command());
+         });
+    },
+
+    load_command_group: function(){
+        $( "#tetry-commands-content" ).load( "/tetry/api/1.0/tasks/"+ $(this).attr("data") );
+        $( ".tetry-command-group" ).parent().attr("class", "");
+        $( this ).parent().attr("class", "active");
+        tetry.add_event_handlers();
+    },
+
+    send_command: function(){
+        tetry.ajaxer("/tetry/api/1.0/tasks/", {name: $(this).text()});
+    },
+
+    ajaxer: function(url, data){
+        $.ajax({
+            url: "post.php",
+            data: data,
+            type: "POST",
+            dataType : "json",
+            success: function( json ) {
+                alert( json.status );
+            },
+            error: function( xhr, status, errorThrown ) {
+                alert( "Sorry, there was a problem!" );
+                console.log( "Error: " + errorThrown );
+                console.log( "Status: " + status );
+                console.dir( xhr );
+            },
+
+            // Code to run regardless of success or failure
+            complete: function( xhr, status ) {
+                //alert( "The request is complete!" );
+            }
+        });
+    }
+};
+
+$(function() {
+    tetry.init();
+});
